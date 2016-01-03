79bc732aa4107567854923d9d17f56e70dc2ed03
Laurent Belcour <laurent.belcour@gmail.com>
2016-01-03 21:57:06 +0100

    Adding first call to WebGL.

--- javascripts/draw_cov_travel.js
+++ javascripts/draw_cov_travel.js
@@ -5,7 +5,24 @@ loadFunction = function() {
       alert("Impossible de récupérer le canvas");
    }
 
-   tr_canvas.style.background = "#FF0";
+   initWebGL(tr_canvas);
+   if (gl) {
+      gl.clearColor(1.0, 1.0, 1.0, 1.0);
+      gl.enable(gl.DEPTH_TEST);
+      gl.depthFunc(gl.LEQUAL);
+      gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+  }
+}
+
+initWebGL = function(canvas) {
+   gl = null;
+   try {
+      gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+   } catch(e) {}
+
+   if (!gl) {
+      alert("Could not init WebGL context.");
+   }
 }
 
 if(addLoadEvent) {
